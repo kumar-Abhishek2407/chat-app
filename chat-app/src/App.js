ce1c212d4dd2b415d8fd9e46f08ef0c12a1424ee
Abhishek Kumar <abhishek24072000@gmail.com>
2023-06-26 15:40:54 +0530

    scaffold the project

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import 'rsuite/dist/styles/rsuite-default.css';
+import './App.css';
 import './styles/main.scss';
+import 'rsuite/dist/styles/rsuite-default.css';
 function App() {
-  return <div>helo</div>;
+  return <div>hello</div>;
 }
 
 export default App;
