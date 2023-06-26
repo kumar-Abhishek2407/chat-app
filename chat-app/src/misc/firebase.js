7d8e24847a943222ea0422c3180585aba5b7718f
Abhishek Kumar <abhishek24072000@gmail.com>
2023-06-26 16:46:37 +0530

    initilise firebase project

---
+++ chat-app/src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDrUPAJqnR7Sngr06zvjon7ZM4HQM6052E',
+  authDomain: 'chat-web-app-4fa55.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-4fa55-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-4fa55',
+  storageBucket: 'chat-web-app-4fa55.appspot.com',
+  messagingSenderId: '70046249317',
+  appId: '1:70046249317:web:dcbb8d46c40a18e69db18c',
+};
