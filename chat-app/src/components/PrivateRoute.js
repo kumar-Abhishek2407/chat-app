bcc449c6b0ffc7d09c209fb56365d0e18fbf4e11
Abhishek Kumar <abhishek24072000@gmail.com>
2023-06-28 14:01:52 +0530

    added profile context

--- chat-app/src/components/PrivateRoute.js
+++ chat-app/src/components/PrivateRoute.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { useProfile } from '../context/profile.context';
 const PrivateRoute = ({ children, ...routeProps }) => {
-  const profile = false;
+  const profile = useProfile();
   if (!profile) {
     return <Redirect to="/signin" />;
   }
