0e34fa770d7926ec3cbb6f77437e8e6a02b436a1
Abhishek Kumar <abhishek24072000@gmail.com>
2023-07-12 19:30:35 +0530

    started building dashboard, added signout button and display user name

--- chat-app/src/components/dashboard/DashboardToggle.js
+++ chat-app/src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
-import { useModalState } from '../../misc/custom-hooks';
+import React, { useCallback } from 'react';
+import { Button, Drawer, Icon, Alert } from 'rsuite';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery('(max-width:992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed out', 4000);
+    close();
+  }, [close]);
   return (
     <>
       <Button block color="blue">
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
