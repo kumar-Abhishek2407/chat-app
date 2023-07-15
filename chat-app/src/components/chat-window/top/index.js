1cfd7202839efda4463cdf755781e26c8741dd0c
Abhishek Kumar <abhishek24072000@gmail.com>
2023-07-15 13:26:57 +0530

    started to build chat top, created room description model

--- chat-app/src/components/chat-window/top/index.js
+++ chat-app/src/components/chat-window/top/index.js
@@ -1,8 +1,37 @@
 import React, { memo } from 'react';
 import { useCurrentRoom } from '../../../context/context-room.context';
+import { ButtonToolbar, Icon } from 'rsuite';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width:992px)');
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappwar">{name}</span>
+        </h4>
+        <ButtonToolbar className="white-sapce:no-wrap">todo</ButtonToolbar>
+        <div className="d-flex justify-content-between align-items-center">
+          <span>todo</span>
+          <RoomInfoBtnModal />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default memo(Top);
