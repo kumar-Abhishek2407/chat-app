47a181f519126728abb2d2f2cb2b3c421a1efab3
Abhishek Kumar <abhishek24072000@gmail.com>
2023-07-16 08:31:50 +0530

    update all user references if data is updated

--- chat-app/src/components/dashboard/AvatarUploadBtn.js
+++ chat-app/src/components/dashboard/AvatarUploadBtn.js
@@ -6,6 +6,7 @@ import AvatarEditor from 'react-avatar-editor';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helper';
 
 const fileInputTypes = '.png,.jpeg,.jpg';
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
@@ -61,10 +62,15 @@ const AvatarUploadBtn = () => {
 
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
 
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
-      userAvatarRef.set(downloadUrl);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
+
+      await database.ref().update(updates);
+
       setIsLoading(false);
       Alert.info('Avatar has been uploaded', 4000);
     } catch (err) {
